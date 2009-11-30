c59fbdf177bb3a816c7138beb9ed7ced08053d52
Nacho Coloma <icoloma@gmail.com>
2009-11-30 15:41:48 +0000

    Added workaround for fields that are not mapped anymore

--- src/test/java/org/simpleds/metadata/ClassMetadataTest.java
+++ src/test/java/org/simpleds/metadata/ClassMetadataTest.java
@@ -59,6 +59,7 @@ public class ClassMetadataTest extends AbstractDatastoreTest {
 		entity.setProperty("date", d);
 		entity.setProperty("int1", Long.valueOf(1));
 		entity.setProperty("int2", Long.valueOf(2));
+		entity.setProperty("xxx", "foobar"); // ignored property that is not mapped
 		Dummy1 dummy = metadata.datastoreToJava(entity);
 		assertEquals(key, dummy.getKey());
 		assertEquals("foo", dummy.getName());
